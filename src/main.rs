032226c67e7daa09744d8c9e2ce7fc413420e00a
Ricardo Dalcin <rdalcin@nelogica.com.br>
2023-08-16 19:30:13 -0300

    :sparkles: Add balloon spawning and looping

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,6 @@ use macroquad::prelude::*;
 
 use std::collections::LinkedList;
 
-const SQUARES: i16 = 16;
-
-type Point = (i16, i16);
-
 enum Direction {
     Right,
     Left,
@@ -24,29 +20,64 @@ async fn main() {
     let mut last_update = get_time();
     let mut game_over = false;
 
-    let mut balloon = Balloon {
-        position: Vec2::new(-screen_width() / 2., 0.),
-        direction: Direction::Right,
-    };
+    let mut balloons: LinkedList<Balloon> = LinkedList::new();
+
+    let mut spawn_timer = 0.;
 
     loop {
         if !game_over {
+            clear_background(LIGHTGRAY);
+
             if is_key_down(KeyCode::T) {
                 game_over = true;
             }
 
             if is_key_down(KeyCode::Right) {
-                balloon.position.x += BALLOON_SPEED * get_frame_time();
-            } else if is_key_down(KeyCode::Left) {
-                balloon.position.x -= BALLOON_SPEED * get_frame_time();
-            }
-        }
-        if !game_over {
-            clear_background(LIGHTGRAY);
+                let text = "Game Over. Press [enter] to play again.";
+                let font_size = 30.;
+                let text_size = measure_text(text, None, font_size as _, 1.0);
 
+                draw_text(
+                    text,
+                    screen_width() / 2. - text_size.width / 2.,
+                    screen_height() / 2. + text_size.height / 2.,
+                    font_size,
+                    DARKGRAY,
+                );
+            }
+
+            if get_time() - last_update > 1. / 60. {
+                last_update = get_time();
+                score += 1;
+            }
+
+            spawn_timer += get_frame_time();
+
+            if (spawn_timer > 1.) {
+                spawn_timer -= 1.;
+                let new_balloon: Balloon = Balloon {
+                    position: Vec2::new(BALLOON_SIZE, screen_height() / 2.),
+                    direction: Direction::Right,
+                };
+                balloons.push_back(new_balloon);
+            }
+
+            for balloon in &mut balloons {
+                match balloon.direction {
+                    Direction::Right => balloon.position.x += BALLOON_SPEED * get_frame_time(),
+                    Direction::Left => balloon.position.x -= BALLOON_SPEED * get_frame_time(),
+                }
+
+                if balloon.position.x > screen_width() + BALLOON_SIZE {
+                    balloon.position.x = -BALLOON_SIZE;
+                } else if balloon.position.x < -BALLOON_SIZE {
+                    balloon.position.x = screen_width() + BALLOON_SIZE;
+                }
+
+                draw_circle(balloon.position.x, screen_height() / 2., BALLOON_SIZE, RED);
+            }
             let game_size = screen_width().min(screen_height());
 
-            draw_circle(balloon.position.x, balloon.position.y, BALLOON_SIZE, RED);
             draw_text(format!("SCORE: {score}").as_str(), 10., 20., 20., DARKGRAY);
         } else {
             clear_background(WHITE);
@@ -63,10 +94,7 @@ async fn main() {
             );
 
             if is_key_down(KeyCode::Enter) {
-                balloon = Balloon {
-                    position: Vec2::new(-screen_width() / 2., 0.),
-                    direction: Direction::Right,
-                };
+                balloons.clear();
                 score = 0;
                 last_update = get_time();
                 game_over = false;
